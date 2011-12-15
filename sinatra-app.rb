926abe540306bd7b54f6d5a7b3819cf71172bbb7
Brian Yang <brianyang1@gmail.com>
2011-12-15 14:11:40 -0500

    demo app

---
+++ sinatra-app.rb
@@ -0,0 +1,43 @@
+require 'rubygems'
+require 'sinatra'
+require 'mongo'
+require 'json'
+
+
+
+get '/' do
+  "handle route and write to db"
+end
+
+get '/db' do
+db = Mongo::Connection.new.db("test")
+coll = db.collection("foo")
+doc = {"tester" => "tested", "info" => {"x" => 203, "y" => '102'}}
+coll.insert(doc)
+'store'
+
+end
+
+get '/example.json' do
+  content_type :json
+  { :key1 => 'value1', :key2 => 'value2' }.to_json
+end
+
+
+get '/save/:topVal/:leftVal/:elemId' do
+
+db = Mongo::Connection.new.db("test")
+coll = db.collection("foo")
+topVal = params[:topVal]
+leftVal = params[:leftVal]
+elemId = params[:elemId]
+doc = {"_id"=>elemId, "top"=> topVal, "left'"=>leftVal}
+coll.save(doc)
+
+  content_type :json
+  { :top => params[:topVal], :left => params[:leftVal] }.to_json  
+  
+end
+
+
+
